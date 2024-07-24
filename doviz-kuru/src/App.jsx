d78830cbc996b2cda251af12606739fabbce19c6
Gulsahbb <gulsahbaskurt.10@gmail.com>
2024-07-24 20:34:44 +0300

    update

--- doviz-kuru/src/App.jsx
+++ doviz-kuru/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Currency from './components/Currency'
 
 function App() {
 
 
   return (
-    <div>
-      Gülşah
+    // Bizim ekranda fotoğraf koyduğumuz alan currency içerisinde.Biz burada bu currency yi div içine yazdığımız için burada da genel kısmı değiştirebilmek için div içerisine style kodu yazdık.
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+      <Currency />
     </div>
   )
 }
--- doviz-kuru/src/components/Currency.jsx
+++ doviz-kuru/src/components/Currency.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
-
+import '../css/currency.css';
+import { FaArrowRightArrowLeft } from "react-icons/fa6";
 function Currency() {
     return (
         // Arayüzü burada tasarlıyoruz.
-        <div>Currency</div>
+        <div className='currency-div'>
+            <div style={{ fontFamily: "Arial", backgroundColor: "black", color: "white", width: "100%", textAlign: "center" }}>
+                <h3>DÖVİZ KURU UYGULAMASI</h3>
+            </div>
+
+            <div style={{ marginTop: "30px" }}>
+                <input type="number" className='amount' />
+
+                <select className='from-currency-option'>
+                    <option>USD</option>
+                    <option>EUR</option>
+                    <option>TL</option>
+                </select>
+                <FaArrowRightArrowLeft style={{ fontSize: "10px", marginRight: "10px" }} />
+                <select className='to-currency-option'>
+                    <option>TL</option>
+                    <option>USD</option>
+                    <option>EUR</option>
+                </select>
+
+                <input type="number" className='result' />
+
+            </div>
+
+            <div>
+                <button className='exchange-button'>ÇEVİR</button>
+            </div>
+
+        </div>
     )
 }
 
