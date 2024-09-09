db5ac900369159bbed689922907f7762d68aaccc
Gulsah BASKURT <gulsahbaskurt.10@gmail.com>
2024-09-09 19:09:59 +0300

    Update App.jsx

--- doviz-kuru-react/src/App.jsx
+++ doviz-kuru-react/src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import Currency from './components/Currency'
 
 function App() {
-  return (
-    <div>
 
+
+  return (
+    // Bizim ekranda fotoğraf koyduğumuz alan currency içerisinde.Biz burada bu currency yi div içine yazdığımız için burada da genel kısmı değiştirebilmek için div içerisine style kodu yazdık.
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+      <Currency />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
